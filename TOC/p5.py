b2059b51ba443362f33f7becc1041e2c2bc23426
VanV <vainavinair2009558@gmail.com>
2023-03-16 15:31:34 +0530

    p5

---
+++ TOC/p5.py
@@ -0,0 +1,46 @@
+# Design a program for Turing machine that’s accepts the even number of 1's.
+
+states = {
+    'A': {
+        '0': ('A', '0', 'R'), 
+        '1': ('B', '0', 'R'), 
+        '_': ('C', '_', 'L')
+    },
+    'B': {
+        '0': ('B', '0', 'R'), 
+        '1': ('A', '0', 'R'), 
+        '_': ('C', '_', 'L')
+    },
+    "C":{}
+}
+
+initial_state = "A"
+final_state = {"A"}
+
+def turing_machine(input_str):
+    current_state = initial_state
+    tape = list(input_str)
+    i_head = 0
+
+    while True:
+        if tape[i_head] not in states[current_state]:
+            return False
+
+        new_state, write_value, move_dir = states[current_state][tape[i_head]]
+        tape[i_head] = write_value
+
+        if move_dir == 'R':
+            i_head += 1
+        elif move_dir == 'L':
+            i_head -= 1
+
+        current_state = new_state
+
+        if current_state in final_state and i_head >= len(tape):
+            return True
+        elif current_state not in states or i_head >= len(tape) or i_head < 0:
+            return False
+
+
+
+print(turing_machine(input("Enter a Stirng: ")))  
