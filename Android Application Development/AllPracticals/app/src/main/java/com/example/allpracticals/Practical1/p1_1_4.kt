4680b2d84864043d7584ab1ec4fe19d161a7192c
VanV <vainavinair2009558@gmail.com>
2023-03-16 17:29:57 +0530

    ADDED PRAC 1.1.4

--- Android Application Development/AllPracticals/app/src/main/java/com/example/allpracticals/Practical1/p1_1_4.kt
+++ Android Application Development/AllPracticals/app/src/main/java/com/example/allpracticals/Practical1/p1_1_4.kt
@@ -1,2 +1,14 @@
 package com.example.allpracticals.Practical1
 
+import java.util.*
+
+fun main(){
+    println("Enter a number: ")
+    var sc = Scanner(System.`in`).nextInt()
+    for(i in 1..12){
+        for (j in 1..sc) {
+            print("${j}*${i}=${i * j}\t")
+        }
+        println()
+    }
+}
